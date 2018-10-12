3c7ecaae02559ed1d268773caa81209e25b33a16
Takumi Nisato <sekaowalike46@icloud.com>
2018-10-12 11:44:23 +0900

    oo

---
+++ CollisionProject1/Colli_Project1/Colli_Project1/ScreenSetings/ScreenSet.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "DxLib.h"
+class ScreenSet {
+
+public:
+
+	void ScreenMode(int WINDOW_WIDTH, int WINDOW_HEIGHT, 
+					int COLOR_BIT, float RATE, int ChangeWindowFlag)
+	{
+		//サイズ設定
+		SetGraphMode(WINDOW_WIDTH, WINDOW_HEIGHT, COLOR_BIT);
+		//描画倍率
+		SetWindowSizeExtendRate(1.0f);
+		//ウィンドウモードにするか
+		ChangeWindowMode(ChangeWindowFlag);
+		SetUseDirect3DVersion(DX_DIRECT3D_9);
+		//描画倍率
+		SetWindowSizeExtendRate(RATE);
+
+	}
+
+	void ScreenBackMode() {
+		//描画先画面を裏画面にセット
+		SetDrawScreen(DX_SCREEN_BACK);
+		SetUseBackCulling(TRUE);
+
+		//計測中に別のウィンドウがアクティブになっても問題が無いように常時実行フラグ
+		SetAlwaysRunFlag(TRUE);
+	}
+
+};
---
+++ CollisionProject1/Colli_Project1/Colli_Project1/main.cpp
@@ -0,0 +1,30 @@
+#include"ScreenSetings/ScreenSet.h"
+// プログラムは WinMain から始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	ScreenSet Screen;
+	//スクリーン関係設定
+	Screen.ScreenMode(1920, 1080, 32, 0.6f, TRUE);
+
+	//Dxlib初期化処理
+	if (DxLib_Init() == -1)return -1;//エラーが起きたら終了
+	//裏描画処理
+	Screen.ScreenBackMode();
+
+
+	SetFontSize(32);
+
+	DrawBox(0, 0, 640, 480, GetColor(255,0,0), FALSE);    // 四角形を描画
+
+	ScreenFlip();	//裏画面の内容を表画面に反映させる
+
+	SetAlwaysRunFlag(FALSE);
+
+	WaitKey();
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+
+
+}
